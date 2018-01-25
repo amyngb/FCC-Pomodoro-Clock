219ab00a015d97ddeca6b6d13d6ac31753e1c93f
Amy Bedinghaus <amyngbedinghaus@gmail.com>
2018-01-24 23:04:25 -0500

    Bind buttons

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
 
-
-
-
-
-
 var app = new Vue ({
     el: "#app",
-    data: [],
-    methods: []
+    data: {
+        Session: 25,
+        BreakTime: 5   
+    },
+    methods: {
+        AddToSession: function() {
+             return this.Session++;
+        },
+
+        SubtractFromSession: function () {
+            if (this.Session > 1) 
+            return this.Session--;
+        }, 
+
+        AddToBreakTime: function() {
+             return this.BreakTime++;
+        },
+
+        SubtractFromBreakTime: function () {
+            if (this.BreakTime > 1) 
+            return this.BreakTime--;
+        }, 
+
+        Reset: function () {
+            return (this.Session = 25, this.BreakTime = 5)
+        },
+
+        StartCountdown: function () {
+            var t = this.Session;
+            var seconds = t * 60;
+            var minutes = t;
+            var hours = t % 60;
+            setInterval(function() {
+                this.Pomodoro.seconds
+            })
+            
+        }
+    },
+    computed: {
+        Pomodoro: function () {
+            var s = this.Session;
+            var pomodoro= {
+                hours: s % 60,
+                minutes: s - (hours * 60),
+                seconds: (s * 60) - (minutes * 60)
+            } 
+            return pomodoro;
+        }
+        //Session:
+        // Clear: window.clearTimeout (this.data.Session)
+
+    }
 });
 
 
